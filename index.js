01b9e435e24ea36bef1871df39c6ac17d60fa2ce
Pasitha <50576299+Pasitha@users.noreply.github.com>
2021-09-16 10:39:40 +0700

    Update index.js
    load birthdayUpdate file

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const config = require('./settings.json');
 require('./utils/loadEvents')(client);
 require('./utils/loadCommands')(client);
 
-require('./utils/yt-birthdayUpdate')(client);
+require('./utils/birthdayUpdate')(client);
+require('./utils/ytUpdate')(client);
 
 client.login(config.token);
