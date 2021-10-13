1430484a1dc59bf649f8d4cfe59bd88153c5bf89
Pasitha <50576299+Pasitha@users.noreply.github.com>
2021-10-13 15:06:44 +0700

    Update birthdayUpdate.js
    add an setInterval every 1 hour

--- utils/birthdayUpdate.js
+++ utils/birthdayUpdate.js
@@ -6,23 +6,25 @@ const config = require('../settings.json');
 let jkc_json_file = require('../database/jkc.json');
 
 module.exports = client => {
-    const todayStr = new Date().toLocaleString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit', timeZone: 'Asia/Bangkok' }).slice(0,5);
-
-    if (config.lastrun !== todayStr) {
-        config.lastrun = todayStr;
-        fs.writeFile('settings.json', jsonstringify(config), (err) => {
-            if (err) throw err;
-        });
-
-        for (let i = 0; i < jkc_json_file.member.length; i++) {
-            if (jkc_json_file.member[i].birthDay.slice(0, 5) === todayStr) {
-                const HBDEmbed = new MessageEmbed()
-                    .setThumbnail(client.user.displayAvatarURL()).setColor("#FFD157")
-                    .setTitle(`🎂🎂🎂 สุขสันต์วันเกิดนะคะ 🥂 ${require('../jkc.json').member[0].youtube.channelName} 🎂🎂🎂`)
-                    .setFooter(client.user.username + " | Version " + config.version, client.user.displayAvatarURL());
-            
-                return client.channels.cache.get("552889042878857227").send({ embeds : [HBDEmbed] });
+    setInterval(() => {
+        const todayStr = new Date().toLocaleString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit', timeZone: 'Asia/Bangkok' }).slice(0,5);
+    
+        if (config.lastrun !== todayStr) {
+            config.lastrun = todayStr;
+            fs.writeFile('settings.json', jsonstringify(config), (err) => {
+                if (err) throw err;
+            });
+    
+            for (let i = 0; i < jkc_json_file.member.length; i++) {
+                if (jkc_json_file.member[i].birthDay.slice(0, 5) === todayStr) {
+                    const HBDEmbed = new MessageEmbed()
+                        .setThumbnail(client.user.displayAvatarURL()).setColor("#FFD157")
+                        .setTitle(`🎂🎂🎂 สุขสันต์วันเกิดนะคะ 🥂 ${require('../jkc.json').member[0].youtube.channelName} 🎂🎂🎂`)
+                        .setFooter(client.user.username + " | Version " + config.version, client.user.displayAvatarURL());
+                
+                    return client.channels.cache.get("552889042878857227").send({ embeds : [HBDEmbed] });
+                }
             }
         }
-    }
+    }, 3600000);
 }
