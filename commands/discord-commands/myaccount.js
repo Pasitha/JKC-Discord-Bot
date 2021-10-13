0ef30b89d62e4271eb7391c1efafb9ae663d5e23
Pasitha <50576299+Pasitha@users.noreply.github.com>
2021-10-13 23:20:38 +0700

    Create myaccount.js
    function for checking account balance

---
+++ commands/discord-commands/myaccount.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+
+const config = require('../../settings.json');
+let account = require('../../database/account.json');
+
+module.exports.run = (client, JKCJrBot, JKCSupBot, message, args) => {
+    if (!Object.keys(account).includes(message.author.id)) {
+        account[message.author.id] = {
+            name: message.author.username,
+            amount: 100
+        }
+    }
+
+    return message.channel.send({ embeds: [new MessageEmbed().setTitle(`🤑บัญชีของ ${message.author.username}🤑`).setDescription(`🎰ตอนนี้คุณ ${message.author.username} มีเงินอยู่ 💲${account[message.author.id].amount} แหน่ะ`)
+        .setThumbnail(client.user.displayAvatarURL()).setColor('#FFD157')
+        .setFooter(client.user.username + ' | Version ' + config.version, client.user.displayAvatarURL())] });
+}
+
+module.exports.config = {
+    name: 'myaccount',
+    aliases: ['account']
+}
