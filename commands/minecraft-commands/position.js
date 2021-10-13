a8cb5da05694d681ef6357ca6e4762d2266206ac
Pasitha <50576299+Pasitha@users.noreply.github.com>
2021-10-13 15:02:51 +0700

    Update and rename pos.js to position.js

--- commands/minecraft-commands/position.js
+++ commands/minecraft-commands/position.js
@@ -2,7 +2,6 @@ const config = require('../../settings.json')
 const { MessageEmbed } = require('discord.js');
 
 module.exports.run = async (client, JKCJrBot, JKCSupBot, message, args) => {
-    
     if (message.guild.id === '423007343127822338') {
         if (!args[0]) return message.channel.send({ embeds: [
             new MessageEmbed().setTitle('Jukucrush junior SS.5').setDescription(`${JKCJrBot.username}`).addField('ตอนนี้ในเซิฟอยู่ทีพิกัด', `x:${JKCJrBot.entity.position.x.toFixed(2)}, y:${JKCJrBot.entity.position.y.toFixed(2)}, z:${JKCJrBot.entity.position.z.toFixed(2)}`)
@@ -25,7 +24,7 @@ module.exports.run = async (client, JKCJrBot, JKCSupBot, message, args) => {
         ]});
     } else if (message.guild.id === '748140372814856292') {
         if (!args[0]) return message.channel.send({ embeds: [
-            new MessageEmbed().setTitle('Jukucrush junior SS.5').setDescription(`${JKCSupBot.username}`).addField('ตอนนี้ในเซิฟอยู่ทีพิกัด', `x:${JKCSupBot.entity.position.x.toFixed(2)}, y:${JKCSupBot.entity.position.y.toFixed(2)}, z:${JKCSupBot.entity.position.z.toFixed(2)}`)
+            new MessageEmbed().setTitle('Supporter Server').setDescription(`${JKCSupBot.username}`).addField('ตอนนี้ในเซิฟอยู่ทีพิกัด', `x:${JKCSupBot.entity.position.x.toFixed(2)}, y:${JKCSupBot.entity.position.y.toFixed(2)}, z:${JKCSupBot.entity.position.z.toFixed(2)}`)
             .setThumbnail(`https://mc-heads.net/avatar/${JKCSupBot.players[JKCSupBot.username].entity.uuid}`).setColor('#FFD157')
             .setFooter(client.user.username + ' | Version ' + config.version, client.user.displayAvatarURL())
         ]});
@@ -39,12 +38,12 @@ module.exports.run = async (client, JKCJrBot, JKCSupBot, message, args) => {
         await JKCSupBot.waitForChunksToLoad();
 
         message.channel.send({ embeds: [
-            new MessageEmbed().setTitle('Jukucrush junior SS.5').setDescription(`${args[0]}`).addField('ตอนนี้ในเซิฟอยู่ทีพิกัด', `\`x : ${position.x.toFixed(2)}\ny : ${position.y.toFixed(2)}\nz : ${position.z.toFixed(2)}\``)
+            new MessageEmbed().setTitle('Supporter Server').setDescription(`${args[0]}`).addField('ตอนนี้ในเซิฟอยู่ทีพิกัด', `\`x : ${position.x.toFixed(2)}\ny : ${position.y.toFixed(2)}\nz : ${position.z.toFixed(2)}\``)
             .setThumbnail(`https://mc-heads.net/avatar/${JKCSupBot.players[args[0]].entity.uuid}`).setColor('#FFD157')
             .setFooter(client.user.username + ' | Version ' + config.version, client.user.displayAvatarURL())
         ]});
     } else {
-        
+        return message.channel.send({ embeds: [new MessageEmbed().setAuthor(`คำสั่งนี้หนูไม่ให้สั่งในห้องนี้นะ!!! ไปสั่งหนูให้ถูกห้องเลยนะ`).setColor('#ff0000')] });
     }
 }
 
