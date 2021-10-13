f9e58e811061462e9b689ec19d0e97d67b58f15f
Pasitha <50576299+Pasitha@users.noreply.github.com>
2021-10-13 15:12:55 +0700

    Update random.js

--- commands/discord-commands/random.js
+++ commands/discord-commands/random.js
@@ -2,35 +2,17 @@ const config = require('../../settings.json')
 const { MessageEmbed } = require("discord.js");
 
 module.exports.run = async (client, JKCJrBot, JKCSupBot, message, args) => {
-    if (!args[0]) {
+    if (!args[0]) 
         return message.channel.send({embeds : [new MessageEmbed().setAuthor(`เลขที่ออกคือเลข ${Math.floor((Math.random() * 100)) + 1} ค่ะ`)]});
-    }
-
-    if (!args[1]) {
-        const maxlazy = parseInt(args[0])
-
-        if (maxlazy === NaN) {
-            // พี่ Max เเก้ด้วย
-            return message.channel.send({embeds : [new MessageEmbed().setAuthor(`idk value`)]});
-        }
-
-        return message.channel.send({embeds : [new MessageEmbed().setAuthor(`เลขที่ออกคือเลข ${Math.floor((Math.random() * maxlazy)) + 1} ค่ะ`)]});
-    }
-
-    // พี่ Max เเก้ด้วย
-
-    const maxlazy0 = parseInt(args[0])
-    const maxlazy1 = parseInt(args[1])
-
-    if (maxlazy0 === NaN || maxlazy1 === NaN) {
-        // พี่ Max เเก้ด้วย
-        return message.channel.send({embeds : [new MessageEmbed().setAuthor(`idk value`)]});
-    }
-
-    message.channel.send({embeds : [new MessageEmbed().setAuthor(`เลขที่ออกคือเลข ${Math.floor((Math.random() * (maxlazy1 - maxlazy0 + 1)) ) + maxlazy0} ค่ะ`)]});
+    else if (!args[1] && !isNaN(args[0])) 
+        return message.channel.send({embeds : [new MessageEmbed().setAuthor(`เลขที่ออกคือเลข ${Math.floor((Math.random() * parseInt(args[0]))) + 1} ค่ะ`)]});
+    else if (!isNaN(args[1]))
+        return message.channel.send({embeds : [new MessageEmbed().setAuthor(`เลขที่ออกคือเลข ${Math.floor((Math.random() * (parseInt(args[1]) - parseInt(args[0]) + 1)) ) + parseInt(args[0])} ค่ะ`)]});
+    else 
+        return message.channel.send({ embeds: [new MessageEmbed().setAuthor(`คือ หนูจะบอกว่าที่คุณใส่มาน่ะมันไม่ใช่ตัวเลขอะค่ะ ช่วยใส่ให้หนูใหม่ได้มั้ยคะ`).setColor('#ff0000')] });
 }
 
 module.exports.config = {
     name: 'random',
-    aliases: []
+    aliases: ['roll']
 }
