8fedd0cdb33277d59699884bad9036a887c33c18
max <50576299+Pasitha@users.noreply.github.com>
2021-10-16 14:25:14 +0700

    Update info.js

--- commands/discord-commands/info.js
+++ commands/discord-commands/info.js
@@ -20,7 +20,7 @@ module.exports.run = async (client, JKCJrBot, JKCSupBot, message, args) => {
         .slice(0, -1);
     
     moment.locale('th');
-    const embeduserinfo = new MessageEmbed()
+    const embeduserinfo = new MessageEmbed()setColor(member.displayHexColor).setFooter(client.user.username + ' | Version ' + config.version, client.user.displayAvatarURL());
     try { embeduserinfo.setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512 })) } catch { }
     try { embeduserinfo.setAuthor('Member ' + `${member.user.username}#${member.user.discriminator}` + ' information', member.user.displayAvatarURL({ dynamic: true })) } catch { }
     try { embeduserinfo.addField('Name:', `\`${member.user.username}#${member.user.discriminator}\``, true) } catch { }
@@ -28,10 +28,8 @@ module.exports.run = async (client, JKCJrBot, JKCSupBot, message, args) => {
     try { embeduserinfo.addField('User id:', `\`${member.id}\``, true) } catch { }
     try { embeduserinfo.addField(`${roles.length} Roles:`, `${roles.length < 10 ? roles.join('\n') : roles.length > 10 ? this.trimArray(roles) : 'ไม่มียศ'}`, true) } catch { }
     try { embeduserinfo.addField('Created at:', `\`${moment(member.user.createdTimestamp).format('LLL')} ${moment(member.user.createdTimestamp).fromNow()}\``, true) } catch { }
-    embeduserinfo.setColor(member.displayHexColor);
-    embeduserinfo.setFooter(client.user.username + ' | Version ' + config.version, client.user.displayAvatarURL());
 
-    message.channel.send({ embeds: [embeduserinfo] })
+    return message.channel.send({ embeds: [embeduserinfo] })
 }
 
 module.exports.config = {
