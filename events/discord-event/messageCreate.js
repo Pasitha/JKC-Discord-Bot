86ac02977a70635375404b24a4a812835a442e8a
Pasitha <pasithacppsom@hotmail.com>
2021-10-19 17:30:24 +0100

    add whitelist for website

--- events/discord-event/messageCreate.js
+++ events/discord-event/messageCreate.js
@@ -1,9 +1,14 @@
-const config = require('../../settings.json')
+const config = require('../../settings.json');
 
 module.exports = async (client, JKCJrBot, JKCSupBot, message) => {
 	const prefix = config.prefix;
 
 	if (message.author.bot || !message.content.startsWith(prefix) || message.channel.type === 'dm') return;
+    if (message.content != '') {
+        if (!message.content.match(/\bhttps?:\/\/\S+/gi).some(urls => [
+			'www.youtube.com', 'www.facebook.com', 'www.cdn.discord.app', 'github.com', 'www.google.com'
+		].includes(new URL(urls).hostname))) return message.delete();
+	}
 	if (message.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`))) return message.channel.send(`Prefix : \`${prefix}\``);
 
 	const messageArray = message.content.split(' ');
