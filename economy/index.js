46a825691f5bfb8d3e467fd27a28324d2a6270a6
Pasitha <pasithacppsom@hotmail.com>
2021-10-24 19:13:10 +0100

    update commands handler

--- economy/index.js
+++ economy/index.js
@@ -1,45 +1,47 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client({
     intents: [
-        "GUILDS",
-        "GUILD_BANS",
-        "GUILD_EMOJIS_AND_STICKERS",
-        "GUILD_INTEGRATIONS",
-        "GUILD_INVITES",
-        "GUILD_MEMBERS",
-        "GUILD_MESSAGES",
-        "GUILD_MESSAGE_REACTIONS",
-        "GUILD_MESSAGE_TYPING",
-        "GUILD_PRESENCES",
-        "GUILD_VOICE_STATES",
-        "GUILD_WEBHOOKS",
-        "DIRECT_MESSAGES",
-        "DIRECT_MESSAGE_REACTIONS",
-        "DIRECT_MESSAGE_TYPING"
+        'GUILDS',
+        'GUILD_BANS',
+        'GUILD_EMOJIS_AND_STICKERS',
+        'GUILD_INTEGRATIONS',
+        'GUILD_INVITES',
+        'GUILD_MEMBERS',
+        'GUILD_MESSAGES',
+        'GUILD_MESSAGE_REACTIONS',
+        'GUILD_MESSAGE_TYPING',
+        'GUILD_PRESENCES',
+        'GUILD_VOICE_STATES',
+        'GUILD_WEBHOOKS',
+        'DIRECT_MESSAGES',
+        'DIRECT_MESSAGE_REACTIONS',
+        'DIRECT_MESSAGE_TYPING'
     ]
 });
 
-const config = require("../setting.json");
+const config = require('../setting.json');
 
-fs.readdir('commands/', (err, files) => {
-    
-    if (err) console.log(err);
-
-    const jsfiles = files.filter(f => f.split('.').pop() === 'js');
-    if (jsfiles.length <= 0) {
-        return console.log(`Bot Couldn\'t Find Commands in folder ${dir}`);
-    }
-    
-    jsfiles.forEach((f, i) => {
-        const module = require(`../commands/${f}`);
+['bank', 'casino'].forEach(dir => {
+    fs.readdir(`${dir}/commands`, (err, files) => {
         
-        module.config.name.forEach(commandName => {
-            client.commands.set(commandName, module);
+        if (err) console.log(err);
+    
+        const jsfiles = files.filter(f => f.split('.').pop() === 'js');
+        if (jsfiles.length <= 0) {
+            return console.log(`Bot Couldn\'t Find Commands in folder ${dir}`);
+        }
+        
+        jsfiles.forEach((f, i) => {
+            const module = require(`../commands/${f}`);
+            
+            module.config.name.forEach(commandName => {
+                client.commands.set(commandName, module);
+            });
         });
     });
-});
+})
 
-client.on("messageCreate", message => { 
+client.on('messageCreate', message => { 
     const prefix = config.prefix;
 
 	if (message.author.bot || !message.content.startsWith(prefix) || message.channel.type === 'dm') return;
