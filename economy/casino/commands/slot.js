f6b429c37d391f51354fdbcd34262ad4e4570cce
Pasitha <pasithacppsom@hotmail.com>
2021-10-30 10:12:22 +0100

    add default setting for command file

---
+++ economy/casino/commands/slot.js
@@ -0,0 +1,9 @@
+const { MessageEmbed } = require("discord.js");
+
+const config = require('../../../settings.json');
+
+module.exports.run = async (client, message, args) => {
+
+};
+
+module.exports.name = ['slot'];
