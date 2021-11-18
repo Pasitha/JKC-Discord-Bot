34aa47b3dbe00a62b553189dc2cb5f8ac09d05cf
Pasitha <pasithacppsom@hotmail.com>
2021-11-19 01:12:42 +0700

    add reward and complete the part of the picture

--- economy/casino/commands/slot.js
+++ economy/casino/commands/slot.js
@@ -19,7 +19,7 @@ module.exports.run = async (client, message, args) => {
             }
         });
     }
-    
+
     const attachment = new MessageAttachment('../picture/jkc-casino/casino.png', 'casino.png');
     let msg = await message.channel.send({files: [attachment]});
     
@@ -28,11 +28,12 @@ module.exports.run = async (client, message, args) => {
     };
 
     const collector = msg.createReactionCollector({ filter , time: 15000 });
-    if (account.coins < 100) {
+    if (account.coins > 100) {
         msg.react('1️⃣').catch(error => console.error('One of the emojis failed to react:', error));
         
         collector.on('collect', async (reaction, user) => {
             if (reaction.emoji.name === '1️⃣') {
+                msg.delete();
                 account.coins -= 100;
                 
                 const canvas = Canvas.createCanvas(960, 540);
@@ -43,39 +44,35 @@ module.exports.run = async (client, message, args) => {
                 context.drawImage(background, 0, 0, canvas.width, canvas.height);
                 context.strokeRect(0, 0, canvas.width, canvas.height);
 
-                const slot1 = Math.floor(Math.random()*10) + 1;
-                const slot2 = Math.floor(Math.random()*10) + 1;
-                const slot3 = Math.floor(Math.random()*10) + 1;
+                const slot1 = Math.floor(Math.random()*7) + 1;
+                const slot2 = Math.floor(Math.random()*7) + 1;
+                const slot3 = Math.floor(Math.random()*7) + 1;
 
-                // const first_slot_wheel = await canvas.loadImage(`../picture/jkc-casino/slot-wheel/${slot1}.png`);
-                // const second_slot_wheel = await canvas.loadImage(`../picture/jkc-casino/slot-wheel/${slot2}.png`);
-                // const third_slot_wheel = await canvas.loadImage(`../picture/jkc-casino/slot-wheel/${slot3}.png`);
+                const first_slot_wheel = await Canvas.loadImage(`../picture/jkc-casino/slot/slot_wheel_${slot1}.png`);
+                const second_slot_wheel = await Canvas.loadImage(`../picture/jkc-casino/slot/slot_wheel_${slot2}.png`);
+                const third_slot_wheel = await Canvas.loadImage(`../picture/jkc-casino/slot/slot_wheel_${slot3}.png`);
 
-                // context.drawImage(first_slot_wheel, 20, 20, 250, 250);
-                // context.drawImage(second_slot_wheel, 40, 20, 250, 250);
-                // context.drawImage(third_slot_wheel, 60, 20, 250, 250);
+                context.drawImage(first_slot_wheel, 205, 127, 96, 143);
+                context.drawImage(second_slot_wheel, 357, 127, 96, 143);
+                context.drawImage(third_slot_wheel, 504, 127, 96, 143);
 
-                // if (slot1 === slot2 || slot2 === slot3 || slot1 === slot3) {
-                //     account.coins += 100*0.5;
-                    const attach = new MessageAttachment(canvas.toBuffer(), 'slot.png');
+                const attach = new MessageAttachment(canvas.toBuffer(), 'slot.png');
 
-                    return message.channel.send({
-                        embed: {
-                            description: "This is some text",
-                            image: {
-                                url: "attachment://twitter.png"
-                            }
-                        },
-                        files: [{
-                            attachment: 'images/twitter.png',
-                            name: 'twitter.png'
-                        }]
-                    });
-                // }
+                message.channel.send({ files: [attach] });
+                if (slot1 === slot2 && slot2 === slot3) {
+                    account.coins += slot1 * (slot1 * 10000);
+                }
+                await prisma.user.update({
+                    where: {
+                        discord_id: message.author.id
+                    },
+                    data: {
+                        ...account
+                    }
+                });
             }
         });
     }
 };
 
 module.exports.name = ['slot'];
-
