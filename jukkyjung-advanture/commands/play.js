e07927c9c97b89882d81b17ed76df3ca4ea51763
Pasitha <pasithacppsom@hotmail.com>
2021-11-24 23:45:48 +0700

    change picture path and command name

--- jukkyjung-advanture/commands/play.js
+++ jukkyjung-advanture/commands/play.js
@@ -1,21 +1,21 @@
 const { MessageEmbed, MessageAttachment } = require("discord.js");
 const Canvas = require('canvas');
 
-const config = require('../../../settings.json');
+const config = require('../../settings.json');
 
 module.exports.run = async (client, message, args) => {
     const canvas = Canvas.createCanvas(960, 540);
-    const context = Canvas.getContext('2d');
+    const context = canvas.getContext('2d');
 
-    const background = await Canvas.loadImage(`../picture/jukkyjung-andavture/background/background-${Math.floor(Math.random() * 2) + 1}.png`);
-    const jukkyjung = await Canvas.loadImage(`../picture/jukkyjung-andavture/jukkyjung.png`);
+    const background = await Canvas.loadImage(`../picture/jukkyjung-adventure/Jukkyjung_adventure_background${Math.floor(Math.random() * 3) + 1}.png`);
+    const jukkyjung = await Canvas.loadImage(`../picture/jkc-discord-bot-fa/Jukubot_FA2.png`);
 
     context.drawImage(background, 0, 0, canvas.width, canvas.height);
-    context.drawImage(jukkyjung, 50, 0, 900, 900);
+    context.drawImage(jukkyjung, 30, 200, 350, 350);
 
     const attachment = new MessageAttachment(canvas.toBuffer(), 'game.png');
 
     message.channel.send({files: [attachment]});
 }
 
-module.exports.name = ['transfer'];
+module.exports.name = ['play'];
