6cb114b075db47f0d8407d9914844d0c555ef265
Pasitha <pasithacppsom@hotmail.com>
2021-11-28 00:18:24 +0700

    use ES6 style

--- economy/bank/commands/purse.js
+++ economy/bank/commands/purse.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
 
-const config = require('../../../settings.json');
+const { version } = require('../../../settings.json');
 
 module.exports.run = async (client, message, args) => {
     let account = await prisma.user.findUnique({
@@ -23,7 +23,7 @@ module.exports.run = async (client, message, args) => {
             .setTitle(`🤑กระเป๋าตังของ ${message.author.username}🤑`)
             .setDescription((account.coins === 0) ? ` - ตอนนี้คุณไม่มีเงินเลยนะคะ ไปทำอะไรมาคะเนี่ย😓` : ` - ตอนนี้คุณมีเงินอยู่ :dollar: ${account.coins} แหน่ะ`)
             .setThumbnail(client.user.displayAvatarURL()).setColor('#FFD156')
-            .setFooter(client.user.username + ' | Version ' + config.version, client.user.displayAvatarURL())]
+            .setFooter(client.user.username + ' | Version ' + version, client.user.displayAvatarURL())]
     });
 }
 
