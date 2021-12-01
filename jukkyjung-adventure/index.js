1a17de782dc7e839940ff055f13c95bd439c1206
Pasitha <pasithacppsom@hotmail.com>
2021-12-02 00:49:18 +0700

    use ES6 style

--- jukkyjung-adventure/index.js
+++ jukkyjung-adventure/index.js
@@ -1,5 +1,5 @@
 const { Client, Intents, Collection } = require('discord.js');
-const client = new Discord.Client({
+const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGE,
@@ -11,7 +11,7 @@ const fs = require('fs');
 
 const { prefix, token } = require('../settings.json');
 
-client.commands = new Discord.Collection();
+client.commands = new Collection();
 
 // load discord commands
 fs.readdir('commands/', (err, files) => {
