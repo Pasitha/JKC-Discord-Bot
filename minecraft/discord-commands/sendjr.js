740468993c23d6316e1c87497aa49e0fb3c8b020
Pasitha <pasithacppsom@hotmail.com>
2022-02-15 09:41:04 +0700

    change the folder name to make it clearer

---
+++ minecraft/discord-commands/sendjr.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require("discord.js");
+
+const config = require('../../settings.json');
+
+module.exports.run = async (client, JKCJrBot, JKCSupBot, message, args) => {
+    if (message.member.roles.cache.some(role => {
+        return role.name === 'Jukucrush Member' ||
+            role.name === 'Jukucrush Junior' || role.name === 'Jukuboost' || role.name === 'Moderator';
+    })) {
+        if (message.channel.id === '423047710413946880') {
+            JKCJrBot.chat(`/tellraw @a [\"<\",{\"text\":\"${message.author.username}\",\"bold\":true,\"color\":\"${message.member.displayHexColor}\"},\"> ${args.map(function (sentence) { return sentence; }).join(' ')}\"]`);
+        } else {
+            return message.channel.send({ embeds: [new MessageEmbed().setAuthor(`คำสั่งนี้หนูไม่ให้สั่งในห้องนี้นะ!!! ไปสั่งหนูให้ถูกห้องเลยนะ`).setColor('#ff0000')] });
+        }
+    } else {
+        return message.channel.send({ embeds: [new MessageEmbed().setAuthor(`ฮั่นแน่!! อยากคุยกับคนในเซิฟใช่ม๊า~~~ ไปเอายศมาให้ได้สิ`).setColor('#ff0000')] });
+    }
+}
+
+module.exports.name = ['sendjr'];
