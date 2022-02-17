ca0919533c41cc7967d6a7e3005c2f419cb782a9
Pasitha <pasithacppsom@hotmail.com>
2022-02-17 17:21:20 +0700

    move help command to help module

---
+++ minecraft/commands/help.js
@@ -0,0 +1,5 @@
+module.exports.run = async (bot, args) => {
+    return bot.chat(`ตอนนี้มีคำสั่งดังนี้ค่ะ [cal, pos, random, sendjkc, transfer] ตอนใช้อย่าลืมใส่ $ ก่อนด้วยนะคะ`);
+}
+
+module.exports.name = ['help', 'h'];
