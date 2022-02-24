57faeb447e528972ac6392da0225cb187225e484
Pasitha <pasithacppsom@hotmail.com>
2022-02-24 12:07:36 +0700

    move sendjkc commands to send modules

---
+++ minecraft/commands/send.js
@@ -0,0 +1,7 @@
+module.exports.run = (bot, args) => {
+    client.guilds.cache.get('423007343127822338').channels.cache.get('423047710413946880').send(`<${username}> ${args.map((sentence) => {
+        return sentence;
+    }).join(' ')}`);
+}
+
+module.exports.name = ['sendjkc']
